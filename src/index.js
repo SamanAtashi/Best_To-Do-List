7f473c994e2d30f4bc8310e43768d9d20a55b374
SamanAtashi <milady00126@gmail.com>
2021-07-20 09:43:41 +0300

    Add Functionality of dynamically appending +Local storage

--- src/index.js
+++ src/index.js
@@ -1,13 +1,48 @@
-import _ from 'lodash';
 import './style.css';
 
-function component() {
-	const element = document.createElement('div');
-    
-	// Lodash, currently included via a script, is required for this line to work
-	element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add("hello");
-	return element;
+const list = [];
+
+let index = 1;
+
+function Build() {
+  const getAdd = document.getElementById('getAdd');
+
+  list.push({
+    description: getAdd.value,
+    completed: false,
+    index,
+  });
+  index += 1;
 }
 
-document.body.appendChild(component());
+function showInList() {
+  const getAdd = document.getElementById('getAdd');
+  const show = document.getElementById('show');
+  const createLi = document.createElement('li');
+  createLi.classList.add('item');
+
+  const createCheckBox = document.createElement('input');
+  createCheckBox.type = 'checkbox';
+  createCheckBox.name = 'name';
+  createCheckBox.id = 'checkboxId';
+  createCheckBox.classList.add('space-right');
+
+  const createLabel = document.createElement('label');
+  createLabel.htmlFor = 'checkboxId';
+  createLabel.appendChild(document.createTextNode(getAdd.value));
+
+  createLi.appendChild(createCheckBox);
+  createLi.appendChild(createLabel);
+  show.appendChild(createLi);
+}
+
+const enterBtn = document.getElementById('getAdd');
+
+enterBtn.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+    // document.getElementById('myBtn').click();
+    showInList();
+    Build();
+  }
+});
