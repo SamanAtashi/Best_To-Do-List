5c6434e3d2ab6fbed7edf3bc20a9419f34d1886f
SamanAtashi <milady00126@gmail.com>
2021-07-22 17:35:38 +0300

    Add CRUD.js + functionality

---
+++ src/CRUD.js
@@ -0,0 +1,15 @@
+// eslint-disable-next-line import/no-cycle
+import { globalIndex, list } from './index.js';
+
+// eslint-disable-next-line import/prefer-default-export
+export function buildToPush() {
+  const getAdd = document.getElementById('getAdd');
+
+  list.push({
+    description: getAdd.value,
+    completed: false,
+    globalIndex,
+  });
+
+  globalIndex += 1;
+}
